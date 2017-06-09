9f4950d98d39486a1b8ac53190f0bbe7d0f26c28
jiaodais <635579172@qq.com>
2017-06-09 20:13:55 +0800

    3.21

--- 3.2/ADC.h
+++
@@ -1,8 +0,0 @@
-#ifndef ADC_H
-#define ADC_H
-#include "LPC11xx.h"
-#include "delay.h"
-void ADC_Init(void);
-void ADC_read(void );
-float CaculTwoPoint(float   x1, float y1, float x2, float y2, float x);
-#endif
